d23037300c6aaf9014458663842794cc4952081b
Mads <cph-mn492@cphbusiness.dk>
2019-01-30 18:15:08 +0100

    init

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,77 @@
+package calculator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import calculator.Calculator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mwn
+ */
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    public CalculatorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void addNumber() {
+        int res = calculator.add(4, 8);
+        assertEquals(12, res);
+    }
+
+    @Test
+    public void subNumber() {
+        int res = calculator.sub(58, 42);
+        assertEquals(16, res);
+    }
+
+    @Test
+    public void mulNumber() {
+        int res = calculator.mul(4, 8);
+        assertEquals(32, res);
+    }
+
+    @Test
+    public void divNumber() {
+        int res = calculator.div(32, 8);
+        assertEquals(4, res);
+    }
+
+    @Test (expected = ArithmeticException.class)
+    public void divNumberArit() {
+        calculator.div(0, 4);
+    }
+}
---
+++ src/test/java/integrationtests/WebCalculatorTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integrationtests;
+
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import utils.HttpClient;
+
+/**
+ *
+ * @author mwn
+ */
+public class WebCalculatorTest {
+
+    HttpClient client = new HttpClient("http://localhost:7777/WebApplication/calculator");
+
+    
+    public WebCalculatorTest() {
+    }
+
+    
+    @Test
+    public void addNumber() throws IOException {
+        String res = client.makeHttpRequest("?operation=add&n1=2&n2=3");
+        assertEquals("Result of: 2+3= 5", res);
+    }
+
+    @Test
+    public void subNumber() throws IOException {
+        String res = client.makeHttpRequest("?operation=sub&n1=8&n2=5");
+        assertEquals("Result of: 8-5= 3", res);
+    }
+
+    @Test
+    public void mulNumber() throws IOException {
+        String res = client.makeHttpRequest("?operation=mul&n1=8&n2=2");
+        assertEquals("Result of: 8*2= 16", res);
+    }
+
+    @Test
+    public void divNumber() throws IOException {
+        String res = client.makeHttpRequest("?operation=div&n1=6&n2=3");
+        assertEquals("Result of: 6/3= 2", res);
+    }
+
+    @Test (expected = ArithmeticException.class)
+    public void divNumberArit() throws IOException {
+        String res = client.makeHttpRequest("?operation=div&n1=6&n2=0");
+    }
+}
