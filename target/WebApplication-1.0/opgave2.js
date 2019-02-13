478a53054a35802a66085042b44e4faf08f12a37
Mads <cph-mn492@cphbusiness.dk>
2019-02-13 14:13:03 +0100

    ca1 done

--- target/WebApplication-1.0/opgave2.js
+++ target/WebApplication-1.0/opgave2.js
@@ -39,7 +39,7 @@ function getId(e) {
 }
 
 function apiCallText(parameters) {
-    var domain = "/WebApplication/calculator?";
+    var domain = "/calculator?";
     var result = document.getElementById("textResult");
     fetch(domain + parameters)
             .then(res => res.text())
@@ -47,7 +47,7 @@ function apiCallText(parameters) {
 }
 
 function apiCallJson(parameters) {
-    var domain = "/WebApplication/calculatorjson?";
+    var domain = "/calculatorjson?";
     var result = document.getElementById("jsonResult");
     fetch(domain + parameters)
             .then(res => res.json())
